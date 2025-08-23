c42914e101c953bff9f76df75b9af4e3ddc70a85
Sasmito Adi <89620643+adioss99@users.noreply.github.com>
2025-08-23 19:08:30 +0700

    lint:fix

--- src/pages/profile-page.tsx
+++ src/pages/profile-page.tsx
@@ -1,12 +1,10 @@
 import { useGetUserProfile } from "@/hooks/use-auth";
 
-const ProfilePage: React.FC = () => {
+export const ProfilePage: React.FC = () => {
   const { data: res, isLoading, error } = useGetUserProfile();
   // console.log("user", res);
-  
+
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Something went wrong</p>;
   return <div>Hi, {res?.data?.name}</div>;
 };
-
-export default ProfilePage;
