fc1d08ac69277aa11ab2296782fb2b5d3bf2f772
Sasmito Adi <89620643+adioss99@users.noreply.github.com>
2025-09-29 10:41:54 +0700

    fix: save my local changes

--- src/hooks/use-n8n.ts
+++ src/hooks/use-n8n.ts
@@ -1,24 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-
-import { usePersistStore } from "@/stores/use-persist";
+import { useMutation } from "@tanstack/react-query";
 
 export const useGetAi = () => {
-  const token = usePersistStore.getState().auth.token;
-  return useQuery({
-    queryKey: ["getAi"],
-    queryFn: async () =>
-      await fetch(
-        "http://localhost:5678/webhook-test/49312989-6463-465f-bf01-e893c1bc100c",
-        {
-          method: "POST",
-          headers: {
-            Authorization: token ? `Bearer ${token}` : "",
-            "Content-Type": "application/json",
-          },
-        }
-      ),
+  const token = import.meta.env.VITE_N8N_KEY;
+  const URL = import.meta.env.VITE_N8N_URL;
+  return useMutation({
+    mutationKey: ["getAi"],
+    mutationFn: async (link: string) => {
+      const res = await fetch(URL, {
+        method: "POST",
+        headers: {
+          n8n_key: token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ link }),
+      });
+      return res;
+    },
     retry: false,
-    refetchOnWindowFocus: false,
-    staleTime: Infinity,
   });
 };
